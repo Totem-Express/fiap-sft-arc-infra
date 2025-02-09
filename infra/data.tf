41fe62f9255ee191ac421372ea6be3d356602651
João Henrique Luciano <jao.luciano8@gmail.com>
2025-02-09 17:57:36 -0300

    Unificando banco de dados e infra neste repositório

--- infra/data.tf
+++
@@ -1,5 +0,0 @@
-data "aws_iam_role" "labrole" {
-  name = "LabRole"
-}
-
-data "aws_availability_zones" "available" {}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,47 +1,40 @@
-variable "prefix" {
-  type    = string
-  default = "totem_express"
-}
-
-variable "cluster_name" {
-  type    = string
-  default = "8soat"
-}
-
-variable "instance_types" {
-  type    = list(string)
-  default = ["t3a.medium"]
-}
-
-variable "subnet_count" {
-  type    = number
-  default = 2
-}
-
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-
-variable "policy_arn" {
-  type    = string
-  default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-}
-
-variable "region" {
+variable "aws_region" {
+  description = "AWS region"
   type        = string
-  description = "AWS Region"
   default     = "us-east-1"
 }
 
-variable "authentication_mode" {
-  description = "The authentication mode for the cluster. Valid values are `CONFIG_MAP`, `API` or `API_AND_CONFIG_MAP`"
+variable "db_version" {
+  description = "Database version"
   type        = string
-  default     = "API_AND_CONFIG_MAP"
 }
 
-variable "principal_arn" {
-  type    = string
-  default = "arn:aws:iam::924978846501:role/voclabs"
+variable "db_instance_name" {
+  description = "Database name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+}
+
+variable "db_instance_class" {
+  description = "RDS instance type"
+  type        = string
+}
+
+variable "db_allocated_storage" {
+  description = "Allocated storage size in GB"
+  type        = number
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
 }
